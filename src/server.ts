e1ba92d4e9ba93afc66787b945349eb07e57a664
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-02-15 07:58:37 -0500

    feat : cors configuration

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,26 @@
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 
 import type { Request, Response } from "express";
 
 dotenv.config();
 
-const app = express();
+const PORT = process.env["PORT"];
 
-const PORT = process.env.PORT || 8001;
+const app = express();
+const corsOption = {
+  origin: process.env["FRONTEND_URL"],
+  // origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
+
+// middleswares
+app.use(express.json());
+app.use(cors(corsOption));
+app.options(/.*/, cors(corsOption));
 
 app.get("/api/v1/", (req: Request, res: Response) => {
   res.json({
