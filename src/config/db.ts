4ad0e0205e535a65f4fedf7aaa8de85fe51b66cf
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-02-15 08:09:09 -0500

    fix : env variables access

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,10 +2,10 @@ import mysql from "mysql2/promise";
 import type { ConnectionOptions } from "mysql2";
 
 const access: ConnectionOptions = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env["DB_HOST"],
+  user: process.env["DB_USER"],
+  password: process.env["DB_PASSWORD"],
+  database: process.env["DB_DATABASE"],
   port: 3306,
 };
 
