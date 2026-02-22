c2f15b924d2def85f119d636e53f91ec1f10d166
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-02-22 15:53:04 -0500

    feat : adding raw products services

--- src/services/raw-products.services.ts
+++ src/services/raw-products.services.ts
@@ -1,16 +1,55 @@
 import { pool } from "../config/pool";
+import type { RowDataPacket, ResultSetHeader } from "mysql2";
+import type { RawProduct } from "../types/type";
 
 const getAllRawProducts = async () => {
-  const [products] = await pool.query("SELECT * FROM RawProducts");
+  const [products] = await pool.query<RowDataPacket[]>(
+    "SELECT * FROM RawProducts",
+  );
 
   return products;
 };
 
-const create = async () => {
-  // const [result] = await pool.query<ResultSetHeader>(
-  //   "INSERT INTO users (name, email) VALUES (?, ?)",
-  //   [data.name, data.email]
-  // );
+const getById = async (id: number) => {
+  const [product] = await pool.query<RowDataPacket[]>(
+    "SELECT * FROM RawProducts WHERE id = ?",
+    [id],
+  );
+
+  return product[0] || null;
 };
 
-export const rawProductService = { getAllRawProducts, create };
+const create = async (data: RawProduct) => {
+  const [result] = await pool.query<ResultSetHeader>(
+    "INSERT INTO RawProducts (name, stock_quantity, unit_of_measure) VALUES (?, ?, ?)",
+    [data.name, data.stock_quantity, data.unit_of_measure],
+  );
+
+  return { id: result.insertId, ...data };
+};
+
+const update = async (id: number, data: Partial<RawProduct>) => {
+  const [result] = await pool.query<ResultSetHeader>(
+    "UPDATE RawProducts SET name = ?, stock_quantity = ?, unit_of_measure = ? WHERE id = ?",
+    [data.name, data.stock_quantity, data.unit_of_measure, id],
+  );
+
+  return result.affectedRows > 0;
+};
+
+const deleteById = async (id: number) => {
+  const [result] = await pool.query<ResultSetHeader>(
+    "DELETE FROM RawProducts WHERE id = ?",
+    [id],
+  );
+
+  return result.affectedRows > 0;
+};
+
+export const rawProductService = {
+  getAllRawProducts,
+  getById,
+  create,
+  update,
+  deleteById,
+};
